473217999f7c37ae96d7dfddce82789436d7c7ad
Audreyaaa <57417548+audreygao@users.noreply.github.com>
2022-03-28 01:16:54 -0700

    Create run.js

---
+++ run.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    /*  settings */
+    $('.venobox').venobox(
+        {
+            framewidth: '940px',        // default: ''
+            frameheight: '400px',       // default: ''
+            border: '1px',             // default: '0'
+            numeratio: true,            // default: false
+            infinigall: true            // default: false
+        }
+    ); 
+    
+});
